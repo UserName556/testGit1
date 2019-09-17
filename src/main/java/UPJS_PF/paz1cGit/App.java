381853331e4c58a59f40cb1bf8ecbcfd1d3debd5
User <User@DESKTOP-97QT8TM>
2019-09-17 09:38:27 +0200

    1. commit

---
+++ src/main/java/UPJS_PF/paz1cGit/App.java
@@ -0,0 +1,13 @@
+package UPJS_PF.paz1cGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
